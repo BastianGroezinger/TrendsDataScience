cf7656b7c0b80f8ae2f95c7bac81ef273765f5a8
BastianGroezinger <bg036@hdm-stuttgart.de>
2020-11-23 16:11:06 +0100

    Projekt strctur and minor changes

--- Task1 Exploration.py
+++ Task1 Exploration.py
@@ -1,24 +1,24 @@
 import pandas as pd
+import os
 
-red1 = pd.read_csv('winequality-red-1.csv', sep=';', decimal = ',')
-red2 = pd.read_csv('winequality-red-2.csv', sep=';', decimal = ',')
+red1 = pd.read_csv('data/winequality-red-1.csv', sep=';', decimal =',')
+red2 = pd.read_csv('data/winequality-red-2.csv', sep=';', decimal =',')
 
-white1 = pd.read_csv('winequality-white-1.csv', sep=';', decimal = ',')
-white2 = pd.read_csv('winequality-white-2.csv', sep=';', decimal = ',')
+white1 = pd.read_csv('data/winequality-white-1.csv', sep=';', decimal =',')
+white2 = pd.read_csv('data/winequality-white-2.csv', sep=';', decimal =',')
 
-red1 = red1.drop(['ID'], axis=1)
 red2 = red2.drop(['ID'], axis=1)
 redall = pd.concat([red1, red2], axis=1, sort=False)
-redall = redall.drop(['flavanoids', 'magnesium', 'lightness'], axis=1)
+redall = redall.drop([1598, 1599, 1600])
 
-white1 = white1.drop(['ID'], axis=1)
 white2 = white2.drop(['ID'], axis=1)
 whiteall = pd.concat([white1, white2], axis=1, sort=False)
-whiteall = whiteall.drop(['flavanoids', 'magnesium', 'lightness'], axis=1)
 
 wineall = pd.concat([redall, whiteall], sort=False)
 
-print(whiteall)
-print(redall)
-print(wineall)
-print(wineall.dtypes)
+print("Weißwein\n", whiteall)
+print("Rotwein\n", redall)
+print("Alle Weine\n", wineall)
+print("Feature Datentypen\n", wineall.dtypes)
+
+wineall.to_csv('data/wineall.csv')
